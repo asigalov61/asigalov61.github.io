f09c0766eaf59614a69a457cbcf7f6cfccf192af
Alex <alexlev61@proton.me>
2024-07-26 09:33:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,4 +53,77 @@ start.onclick = async () => {
     const seq = new Sequencer([new MIDI(mid)], synth);
     seq.play();
     title.innerText = "Playing"
+
+    // ===========================================================================================================
+
+    const canvas = document.getElementById("canvas");                         // get canvas
+    const drawingContext = canvas.getContext("2d");
+    /**
+     * create the AnalyserNodes for the channels
+     */
+    const analysers = [];
+    for (let i = 0; i < 16; i++) {
+        analysers.push(context.createAnalyser()); // create analyser
+    }
+
+    // connect them to the synthesizer
+    synth.connectIndividualOutputs(analysers);
+
+    // render analysers in a 4x4 grid
+    function render()
+    {
+        // clear the rectangle
+        drawingContext.clearRect(0, 0, canvas.width, canvas.height);
+        analysers.forEach((analyser, channelIndex) => {
+            // calculate positions
+            const width = canvas.width / 4;
+            const height = canvas.height / 4;
+            const step = width / analyser.frequencyBinCount;
+            const x = width * (channelIndex % 4); // channelIndex % 4 gives us 0 to 2 range
+            const y = height * Math.floor(channelIndex / 4) + height / 2;
+
+            // draw the waveform
+            const waveData = new Float32Array(analyser.frequencyBinCount);
+            // get the data from analyser
+            analyser.getFloatTimeDomainData(waveData);
+            drawingContext.beginPath();
+            drawingContext.moveTo(x, y);
+            for (let i = 0; i < waveData.length; i++)
+            {
+                drawingContext.lineTo(x + step * i, y + waveData[i] * height);
+            }
+            drawingContext.stroke();
+        });
+
+        // draw again
+        requestAnimationFrame(render);
+    }
+    render();
+
+    // create a keyboard
+    const keyboard = document.getElementById("keyboard");
+    // create an array of 128 keys
+    const keys = [];
+    for (let i = 0; i < 128; i++)
+    {
+        const key = document.createElement("td");
+        key.style.width = "5px";
+        key.style.height = "50px";
+        key.style.border = "solid black 1px";
+        keyboard.appendChild(key);
+        keys.push(key);
+    }
+
+    // add listeners to show keys being pressed
+
+    // add note on listener
+    synth.eventHandler.addEvent("noteon", "demo-keyboard-note-on", event => {
+        keys[event.midiNote].style.background = "green"
+    });
+
+    // add note off listener
+    synth.eventHandler.addEvent("noteoff", "demo-keyboard-note-off", event => {
+        keys[event.midiNote].style.background = "white";
+    })
+    
 }
